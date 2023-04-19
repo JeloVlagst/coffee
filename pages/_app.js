241465f8494622235e618f7e9083c59e6b6eec4b
Chris <jelovlagst@gmail.com>
2023-04-20 00:56:44 +0100

    fixed font issue

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
 import '@/styles/globals.css'
+import { Edu_VIC_WA_NT_Beginner } from 'next/font/google'
+
+const edu_vic = Edu_VIC_WA_NT_Beginner({subsets:['latin']})
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <main className={edu_vic.className}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
